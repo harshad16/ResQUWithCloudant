4f6737d1dac53b6200055c29991f61373c7880b6
samba693 <sambasivarao693@gmail.com>
2018-10-21 03:04:39 -0400

    Embedding Visualisation

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import xlrd
+import numpy as np
+from plotly.offline import plot,iplot
+import plotly.figure_factory as ff
+
 import atexit
 import os
 import json
@@ -18,6 +24,9 @@ from cloudant.adapters import Replay429Adapter
 from algoliasearch import algoliasearch
 
 
+#Importing the flood risk data
+sheetname="NOAA"
+noaa = pd.read_excel("./Datasets/DataVizFloodsFV3_22_2017.xlsx",sheet_name=sheetname)
 
 # algolia app connection
 client = algoliasearch.Client(API().app_id, API().admin_api_key)
@@ -199,6 +208,36 @@ def sos():
 			return render_template('index.html', msg='Succesfully SOS Help Request Sent!')
 	return render_template('index.html', error='SOS Request Unable to Sent!') 
 
+@app.route('/api/v1/maps',methods=['GET','POST'])
+def state_flood_events():
+	state_name = request.form.get('state_name')
+	noaa1 = noaa.copy(deep=True)
+	del noaa1['Year']
+	del noaa1['Lat']
+	del noaa1['Lon']
+	del noaa1['CountyZone']
+	df = return_fips(state_name)
+	df2 = pd.merge(noaa1, df, on=['State','County'])
+	del df2['State']
+	fips = df2['FIPS'].tolist()
+	df2 = df2.groupby(['FIPS','County'], as_index=False)['NumEpisodes'].sum()
+	fips = df2['FIPS'].tolist()
+	values = df2['NumEpisodes'].tolist()
+	endpts = list(np.mgrid[min(values):max(values):4j])
+	colorscale = ["#030512","#1d1d3b","#323268","#3d4b94","#3e6ab0",
+					"#4989bc","#60a7c7","#85c5d3","#b7e0e4","#eafcfd"]
+	fig = ff.create_choropleth(
+		fips=fips, values=values, scope=[state_name], show_state_data=True,
+		colorscale=colorscale, binning_endpoints=endpts, round_legend_values=True,
+		plot_bgcolor='rgb(229,229,229)',
+		paper_bgcolor='rgb(229,229,229)',
+		legend_title='Flood risk by county in '+state_name,
+		county_outline={'color': 'rgb(255,255,255)', 'width': 0.5}
+	)
+	division_header = plot(fig,output_type='div')
+	return render_template('index.html',division_header=division_header)
+
+
 def get_precausions(lang="English"):
 	precausions = []
 	selector = {'language': lang,'type':'Precausions'}
@@ -225,7 +264,6 @@ def get_location(latlng):
 
 def get_latlng():
 	g = geocoder.ip('me')
-	print(g.latlng)
 	return ''.join(list(map(str, g.latlng)))
 
 def send_alert(body):
@@ -250,6 +288,18 @@ def update_all_objects():
 		ema.append(result)
 	res = index.add_objects(ema)
 	return True
+
+
+def return_fips(state_name):
+    df = pd.read_csv('./Datasets/'+state_name+'.csv')
+    df['state_FIPS'] = df['state_FIPS'].apply(lambda x:str(x).zfill(2))
+    df['county_FIPS']= df['county_FIPS'].apply(lambda x:str(x).zfill(3))
+    del df['classfp']
+    df['State'] = state_name
+    df['FIPS'] = df['state_FIPS']+df['county_FIPS']
+    del df['state_FIPS']
+    del df['county_FIPS']
+    return df
 
 
 @atexit.register
