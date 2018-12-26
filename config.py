702856ecf95ccc591df0e5ef86b4adb7532be22f
Harshad Reddy Nalla <harshadreddy16@gmail.com>
2018-12-26 18:25:07 +0530

    added empty config file

---
+++ config.py
@@ -0,0 +1,13 @@
+class API():
+	def __init__(self):
+		# Google cloud
+		self.API_KEY = ''
+		# Twillio
+		self.account_sid = ''
+		self.auth_token = ''
+		# Phone number
+		self.to = ''
+		self.from_ = ''
+		# Algolia
+		self.app_id = ""
+		self.admin_api_key = ''
